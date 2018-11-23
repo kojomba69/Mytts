6068524b86e625cd42198f3506d10fb6bb40d1e9
kojomba69 <kojomba@naver.com>
2018-11-23 16:19:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/edu/mytts/subweb.java
@@ -0,0 +1,13 @@
+package com.example.edu.mytts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class subweb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subweb);
+    }
+}
